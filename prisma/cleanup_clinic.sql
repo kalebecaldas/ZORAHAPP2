4cb47b129ddea76453ad5e7e1c08e0f9f2d8eea5
Kalebe Caldas <kalebe.caldas@hotmail.com>
2025-11-26 09:17:40 -0400

    tent 4

--- prisma/cleanup_clinic.sql
+++ prisma/cleanup_clinic.sql
@@ -1,14 +1,3 @@
 DROP TABLE IF EXISTS "InsuranceProcedure" CASCADE;
 DROP TABLE IF EXISTS "InsuranceCompany" CASCADE;
 DROP TABLE IF EXISTS "Procedure" CASCADE;
-
-{
-  "cells": [],
-  "metadata": {
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "nbformat": 4,
-  "nbformat_minor": 2
-}
